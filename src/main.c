709e18da2375cfc8a40a3c7e7c13bd36b401c300
Matheus Almeida <mat.almeida@live.com>
2021-02-11 00:19:12 -0300

    feat: show sprite in screen

--- src/main.c
+++ src/main.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 #include <gb/gb.h>
+#include "./assets/Face.c"
+#include "./utils/math.h"
+#include "./constants/screen.h"
 
 void main() {
-    while(1) {
-        printf("Hello World\n");
-    }
+    set_sprite_data(0, 1, Face);
+    set_sprite_tile(0, 0);
+    SHOW_SPRITES;
+
+    UINT8 spriteX = MIDDLE_X;
+    UINT8 spriteY = MIDDLE_Y;
+
+    move_sprite(0, spriteX, spriteY);
+
 }
