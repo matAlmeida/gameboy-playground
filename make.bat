49860011e603f78a84d8f2d70f01a6cca441ac27
Matheus Almeida <mat.almeida@live.com>
2021-02-10 23:55:19 -0300

    chore: make "make" script quieter

--- make.bat
+++ make.bat
@@ -1,14 +1,16 @@
+@echo off
+
 cd src
 
 C:\GB\gbdk\bin\lcc -Wa-l -Wl-m -Wf--debug -Wl-y -DUSE_SFR_FOR_REG -c -o main.o main.c
 C:\GB\gbdk\bin\lcc -Wa-l -Wl-m -Wf--debug -Wl-y -DUSE_SFR_FOR_REG -o main.gb main.o
 
-move    "main.adb" "..\dist"
-move    "main.asm" "..\dist"
-move    "main.cdb" "..\dist"
-move    "main.gb"  "..\dist"
-move    "main.ihx" "..\dist"
-move    "main.lst" "..\dist"
-move    "main.map" "..\dist"
-move    "main.o"   "..\dist"
-move    "main.sym" "..\dist"
+move    "*.adb" "..\dist"   >nul
+move    "*.asm" "..\dist"   >nul
+move    "*.cdb" "..\dist"   >nul
+move    "*.gb"  "..\dist"   >nul
+move    "*.ihx" "..\dist"   >nul
+move    "*.lst" "..\dist"   >nul
+move    "*.map" "..\dist"   >nul
+move    "*.o"   "..\dist"   >nul
+move    "*.sym" "..\dist"   >nul
