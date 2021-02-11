0db552ce2dce48553628c30a3c3c8cebd28d4e37
Matheus Almeida <mat.almeida@live.com>
2021-02-11 00:18:47 -0300

    feat: add screen constants

---
+++ src/constants/screen.h
@@ -0,0 +1,12 @@
+#ifndef SCREEN_H_
+#define SCREEN_H_
+
+#define MIN_X   8
+#define MAX_X   160
+#define MIDDLE_X (MAX_X / 2) + (MIN_X / 2)
+
+#define MIN_Y   16
+#define MAX_Y   152
+#define MIDDLE_Y (MAX_Y / 2) + (MIN_Y / 2)
+
+#endif // SCREEN_H_
