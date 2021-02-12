684860c8a1a5db804ea997da23ce65e551afab0f
Matheus Almeida <mat.almeida@live.com>
2021-02-11 21:15:16 -0300

    feat: add jump mechanic

--- src/main.c
+++ src/main.c
@@ -1,17 +1,50 @@
 #include <stdio.h>
 #include <gb/gb.h>
-#include "./assets/Face.c"
-#include "./utils/math.h"
+
+/* CONSTANTS */
 #include "./constants/screen.h"
+#include "./constants/acceleration.h"
+
+/* LIBRARIES */
+#include "./libs/jump.h"
+
+/* UTILITY */
+#include "./utils/math.h"
+#include "./utils/delay.h"
+
+/* ASSETS */
+#include "./assets/Face.c"
+
+BYTE is_jumping = 0;
+UINT8 current_y_speed;
+INT16 current_floor_y = 139;
 
 void main() {
     set_sprite_data(0, 1, Face);
     set_sprite_tile(0, 0);
+    DISPLAY_ON;
     SHOW_SPRITES;
 
-    UINT8 spriteX = MIDDLE_X;
-    UINT8 spriteY = MIDDLE_Y;
+    UINT8 face_position[2];
+    face_position[0] = MIDDLE_X;
+    face_position[1] = current_floor_y;
 
-    move_sprite(0, spriteX, spriteY);
+    move_sprite(0, face_position[0], face_position[1]);
 
+    while (1)
+    {
+        if ((joypad() & J_UP) || is_jumping == 1) {
+            air_jump(&is_jumping, &current_y_speed, &current_floor_y, 0, face_position);
+        }
+        if (joypad() & J_LEFT) {
+            face_position[0] = face_position[0] - AIR_GRAVITY;
+            move_sprite(0,face_position[0],face_position[1]);
+        }
+        if (joypad() & J_RIGHT) {
+            face_position[0] = face_position[0] + AIR_GRAVITY;
+            move_sprite(0,face_position[0],face_position[1]);
+        }
+
+        delay_r(4);
+    }
 }
