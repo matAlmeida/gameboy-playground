b5c080fa8e9a1541c90dc58c1ef799ba17bb97cc
Matheus Almeida <mat.almeida@live.com>
2021-02-11 23:56:07 -0300

    wip

--- src/main.c
+++ src/main.c
@@ -7,62 +7,41 @@
 
 /* LIBRARIES */
 #include "./libs/jump.h"
+#include "./libs/sprite.h"
 
 /* UTILITY */
 #include "./utils/math.h"
 #include "./utils/delay.h"
 
 /* ASSETS */
-#include "./assets/Face.c"
+#include "./assets/Sprites.c"
 
 BYTE is_jumping = 0;
 UINT8 current_y_speed;
 INT16 current_floor_y = 139;
 
 void main() {
-    set_sprite_data(0, 1, Face);
-    set_sprite_tile(0, 0);
+    set_sprite_data(0, 4, Sprites);
+
+    struct Sprite* newPlayer = create_sprite(MIDDLE_X, current_floor_y, 1, 3, 0);
+
     DISPLAY_ON;
     SHOW_SPRITES;
 
-    UINT8 face_position[2];
-    face_position[0] = MIDDLE_X;
-    face_position[1] = current_floor_y;
-
-    move_sprite(0, face_position[0], face_position[1]);
-
     while (1)
     {
-        /* Option Keys -> Shift & Return keys on Keyboard */
-        if((joypad() & J_SELECT)) {
-
-        }
-        if((joypad() & J_START)) {
-
-        }
-
-        /* DPAD Keys -> Arrow Keys on Keyboard */
-        if ((joypad() & J_UP) || is_jumping == 1) {
-            air_jump(&is_jumping, &current_y_speed, &current_floor_y, 0, face_position);
-        }
-        if (joypad() & J_DOWN) {
-
-        }
         if (joypad() & J_RIGHT) {
-            face_position[0] = face_position[0] + AIR_GRAVITY;
-            move_sprite(0,face_position[0],face_position[1]);
+            newPlayer->x = newPlayer->x + AIR_GRAVITY;
+            move_sprite_s(newPlayer, newPlayer->x, newPlayer->y);
         }
         if (joypad() & J_LEFT) {
-            face_position[0] = face_position[0] - AIR_GRAVITY;
-            move_sprite(0,face_position[0],face_position[1]);
+            newPlayer->x = newPlayer->x - AIR_GRAVITY;
+            move_sprite_s(newPlayer, newPlayer->x, newPlayer->y);
         }
 
-        /* Action Keys -> A & S keys on Keyboard */
         if((joypad() & J_B)) {
-
         }
         if((joypad() & J_A)) {
-
         }
 
         delay_r(4);
